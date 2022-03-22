dbdeecf9ddc962de735652ae9e5729de0e58201c
Nour Bsoul <74349478+noorbsoul@users.noreply.github.com>
2022-03-22 15:02:16 -0700

    Add files via upload

---
+++ Notebook.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <vector>
+using namespace std;
+#include "Direction.hpp"
+#include "Notebook.hpp"
+using ariel ::Direction ;
+ namespace ariel {
+     void Notebook::write(unsigned int i , unsigned int j ,unsigned c, Direction d, std::string const &ad){
+
+     }
+     std::string Notebook::read(unsigned int i , unsigned int j,  unsigned c , Direction d , unsigned l){
+         return "notebook";
+     }
+     void Notebook::erase (unsigned int i , unsigned int j,  unsigned c , Direction d , unsigned l){
+
+     }
+     void Notebook::show(unsigned int i){
+
+     }
+
+ }
